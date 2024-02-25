89372d620f4759ab08f8d6e27600fed2552fae5f
Jared Winter <jwinternet@outlook.com>
2024-02-25 15:00:10 -0500

    A work in progress.

--- iron_fleet/models.py
+++ iron_fleet/models.py
@@ -25,3 +25,14 @@ class Entry(models.Model):
     def __str__(self):
         """Return a string representing the entry."""
         return f"{self.text[:50]}..."
+
+
+class Idea(models.Model):
+    """An Idea the user is thinking about."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
--- iron_fleet/views.py
+++ iron_fleet/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 
-from .models import Topic, Entry
+from .models import Topic, Entry, Idea
 from .forms import TopicForm, EntryForm
 
 
